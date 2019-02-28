06c969f7ec7a97f8f3f5200e71bdf2167cc7bead
skomulai <skomulai@dummy.not>
2019-02-28 23:41:09 +0200

    One seda queue test

--- src/main/kotlin/com/example/demo/routes/DemoRoute.kt
+++ src/main/kotlin/com/example/demo/routes/DemoRoute.kt
@@ -1,6 +1,9 @@
 package com.example.demo.routes
 
+import org.apache.camel.Endpoint
 import org.apache.camel.EndpointInject
+import org.apache.camel.Exchange
+import org.apache.camel.ExchangePattern
 import org.apache.camel.builder.RouteBuilder
 import org.springframework.stereotype.Component
 
@@ -9,18 +12,22 @@ import org.springframework.stereotype.Component
 class HelloRoute : RouteBuilder() {
 
     @EndpointInject(uri = "{{input}}")
-    lateinit var fromid: String
+    lateinit var fromid: Endpoint
 
     @EndpointInject(uri = "{{output1}}")
-    lateinit var toid1: String
+    lateinit var toid1: Endpoint
 
     @EndpointInject(uri = "{{output2}}")
-    lateinit var toid2: String
+    lateinit var toid2: Endpoint
 
     @Throws(Exception::class)
     override fun configure() {
         from(fromid)
             .log("I'm in the Camel Route!")
+            .to("log:before?showBody=true")
+            .to(ExchangePattern.InOnly, toid1)
+            .process { e -> e.getIn().body = "number 5" }
+            .to("log:after?showBody=true")
             .to(toid2)
     }
 
--- src/test/kotlin/com/example/demo/routes/HelloRouteTest.kt
+++ src/test/kotlin/com/example/demo/routes/HelloRouteTest.kt
@@ -1,48 +1,51 @@
 package com.example.demo.routes
 
-import org.apache.camel.test.spring.CamelSpringBootRunner
-import org.apache.camel.test.spring.DisableJmx
-import org.apache.camel.test.spring.MockEndpoints
-import org.junit.Test
-import org.junit.runner.RunWith
-import org.springframework.test.annotation.DirtiesContext
-import org.springframework.boot.test.context.SpringBootTest
-import org.apache.camel.component.mock.MockEndpoint
+import org.apache.camel.CamelContext
+import org.apache.camel.Endpoint
 import org.apache.camel.EndpointInject
 import org.apache.camel.ProducerTemplate
-import org.apache.camel.CamelContext
+import org.apache.camel.component.mock.MockEndpoint
+import org.apache.camel.test.spring.CamelSpringBootRunner
+import org.apache.camel.test.spring.MockEndpoints
+import org.junit.Test
+import org.junit.Assert.*
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.annotation.DirtiesContext
+import org.apache.camel.component.seda.SedaEndpoint
 import org.springframework.beans.factory.annotation.Autowired
-
-
-
-
-
-
 
 
 @SpringBootTest
 @RunWith(CamelSpringBootRunner::class)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
-@DisableJmx(true)
 @MockEndpoints
 class HelloRouteTest {
+    val inputPayload = "my message"
+    val outputPayload = "number 5"
 
-    val TEST_MESSAGE_PAYLOAD = "my message"
+    @Autowired
+    lateinit var camelContext: CamelContext
 
     @EndpointInject(uri = "{{output2}}")
     lateinit var resultEndpoint: MockEndpoint
 
+//    @EndpointInject(uri = "{{output1}}")
+//    lateinit var sedaEndpoint: SedaEndpoint
+
     @EndpointInject(uri = "{{input}}")
     lateinit var producer: ProducerTemplate
 
-    @Autowired
-    val camelContext: CamelContext? = null
-
     @Test
     fun testInOut() {
-        resultEndpoint!!.expectedMessageCount(1)
-        resultEndpoint!!.message(0).equals(TEST_MESSAGE_PAYLOAD)
-        producer!!.sendBody(TEST_MESSAGE_PAYLOAD)
+        resultEndpoint.expectedMessageCount(1)
+        resultEndpoint.expectedBodiesReceived(outputPayload)
+        producer.sendBody(inputPayload)
+
+        val sedaEndpoint = camelContext.getEndpoint("{{output1}}", SedaEndpoint::class.java)
+
+        val size = sedaEndpoint.exchanges.size
+        assertSame(1, size)
 
         resultEndpoint.assertIsSatisfied()
 
